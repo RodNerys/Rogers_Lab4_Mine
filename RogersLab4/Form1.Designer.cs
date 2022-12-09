df8eda23aa3bba4c7b0e13a786cd4962f9a3810d
Rodrigo Nerys <nerys.rodrigo@gmail.com>
2022-12-09 09:40:43 -0400

    Initial commit

---
+++ RogersLab4/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace RogersLab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpBook = new System.Windows.Forms.GroupBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radCreditCard = new System.Windows.Forms.RadioButton();
+            this.radCash = new System.Windows.Forms.RadioButton();
+            this.grpLocation = new System.Windows.Forms.GroupBox();
+            this.chkFlight = new System.Windows.Forms.CheckBox();
+            this.radMexico = new System.Windows.Forms.RadioButton();
+            this.radFlorida = new System.Windows.Forms.RadioButton();
+            this.radCuba = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.txtPeople = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.grpTripInformation = new System.Windows.Forms.GroupBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.grpBook.SuspendLayout();
+            this.grpLocation.SuspendLayout();
+            this.grpTripInformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBook
+            // 
+            this.grpBook.Controls.Add(this.lblPrice);
+            this.grpBook.Controls.Add(this.label2);
+            this.grpBook.Controls.Add(this.radCreditCard);
+            this.grpBook.Controls.Add(this.radCash);
+            this.grpBook.Controls.Add(this.grpLocation);
+            this.grpBook.Controls.Add(this.label3);
+            this.grpBook.Controls.Add(this.btnBook);
+            this.grpBook.Controls.Add(this.txtPeople);
+            this.grpBook.Controls.Add(this.btnReset);
+            this.grpBook.Location = new System.Drawing.Point(12, 12);
+            this.grpBook.Name = "grpBook";
+            this.grpBook.Size = new System.Drawing.Size(200, 310);
+            this.grpBook.TabIndex = 1;
+            this.grpBook.TabStop = false;
+            this.grpBook.Text = "Book";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPrice.Location = new System.Drawing.Point(55, 235);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(139, 25);
+            this.lblPrice.TabIndex = 0;
+            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(6, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Price:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // radCreditCard
+            // 
+            this.radCreditCard.AutoSize = true;
+            this.radCreditCard.Location = new System.Drawing.Point(9, 188);
+            this.radCreditCard.Name = "radCreditCard";
+            this.radCreditCard.Size = new System.Drawing.Size(101, 19);
+            this.radCreditCard.TabIndex = 3;
+            this.radCreditCard.TabStop = true;
+            this.radCreditCard.Text = "Credit Card";
+            this.radCreditCard.UseVisualStyleBackColor = true;
+            // 
+            // radCash
+            // 
+            this.radCash.AutoSize = true;
+            this.radCash.Location = new System.Drawing.Point(9, 213);
+            this.radCash.Name = "radCash";
+            this.radCash.Size = new System.Drawing.Size(58, 19);
+            this.radCash.TabIndex = 3;
+            this.radCash.TabStop = true;
+            this.radCash.Text = "Cash";
+            this.radCash.UseVisualStyleBackColor = true;
+            // 
+            // grpLocation
+            // 
+            this.grpLocation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.grpLocation.Controls.Add(this.chkFlight);
+            this.grpLocation.Controls.Add(this.radMexico);
+            this.grpLocation.Controls.Add(this.radFlorida);
+            this.grpLocation.Controls.Add(this.radCuba);
+            this.grpLocation.Location = new System.Drawing.Point(9, 52);
+            this.grpLocation.Name = "grpLocation";
+            this.grpLocation.Size = new System.Drawing.Size(185, 130);
+            this.grpLocation.TabIndex = 2;
+            this.grpLocation.TabStop = false;
+            this.grpLocation.Text = "Select Location";
+            // 
+            // chkFlight
+            // 
+            this.chkFlight.AutoSize = true;
+            this.chkFlight.Enabled = false;
+            this.chkFlight.Location = new System.Drawing.Point(6, 100);
+            this.chkFlight.Name = "chkFlight";
+            this.chkFlight.Size = new System.Drawing.Size(122, 19);
+            this.chkFlight.TabIndex = 3;
+            this.chkFlight.Text = "Flight Included";
+            this.chkFlight.UseVisualStyleBackColor = true;
+            // 
+            // radMexico
+            // 
+            this.radMexico.AutoSize = true;
+            this.radMexico.Location = new System.Drawing.Point(6, 75);
+            this.radMexico.Name = "radMexico";
+            this.radMexico.Size = new System.Drawing.Size(71, 19);
+            this.radMexico.TabIndex = 2;
+            this.radMexico.TabStop = true;
+            this.radMexico.Text = "Mexico";
+            this.radMexico.UseVisualStyleBackColor = true;
+            this.radMexico.CheckedChanged += new System.EventHandler(this.radMexico_CheckedChanged);
+            // 
+            // radFlorida
+            // 
+            this.radFlorida.AutoSize = true;
+            this.radFlorida.Location = new System.Drawing.Point(6, 50);
+            this.radFlorida.Name = "radFlorida";
+            this.radFlorida.Size = new System.Drawing.Size(71, 19);
+            this.radFlorida.TabIndex = 1;
+            this.radFlorida.TabStop = true;
+            this.radFlorida.Text = "Florida";
+            this.radFlorida.UseVisualStyleBackColor = true;
+            this.radFlorida.CheckedChanged += new System.EventHandler(this.radFlorida_CheckedChanged);
+            // 
+            // radCuba
+            // 
+            this.radCuba.AutoSize = true;
+            this.radCuba.Location = new System.Drawing.Point(6, 25);
+            this.radCuba.Name = "radCuba";
+            this.radCuba.Size = new System.Drawing.Size(59, 19);
+            this.radCuba.TabIndex = 0;
+            this.radCuba.TabStop = true;
+            this.radCuba.Text = "Cuba";
+            this.radCuba.UseVisualStyleBackColor = true;
+            this.radCuba.CheckedChanged += new System.EventHandler(this.radCuba_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(6, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "People:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnBook
+            // 
+            this.btnBook.Location = new System.Drawing.Point(108, 269);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(75, 35);
+            this.btnBook.TabIndex = 6;
+            this.btnBook.Text = "&Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
+            // 
+            // txtPeople
+            // 
+            this.txtPeople.Location = new System.Drawing.Point(74, 23);
+            this.txtPeople.Name = "txtPeople";
+            this.txtPeople.Size = new System.Drawing.Size(94, 23);
+            this.txtPeople.TabIndex = 1;
+            this.txtPeople.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(15, 267);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 35);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // grpTripInformation
+            // 
+            this.grpTripInformation.Controls.Add(this.btnConfirm);
+            this.grpTripInformation.Controls.Add(this.lblDisplay);
+            this.grpTripInformation.Location = new System.Drawing.Point(218, 12);
+            this.grpTripInformation.Name = "grpTripInformation";
+            this.grpTripInformation.Size = new System.Drawing.Size(264, 312);
+            this.grpTripInformation.TabIndex = 1;
+            this.grpTripInformation.TabStop = false;
+            this.grpTripInformation.Text = "Trip Infromation";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(97, 269);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 35);
+            this.btnConfirm.TabIndex = 0;
+            this.btnConfirm.Text = "&Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblDisplay.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(6, 19);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(252, 235);
+            this.lblDisplay.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnBook;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(494, 336);
+            this.Controls.Add(this.grpTripInformation);
+            this.Controls.Add(this.grpBook);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Booking by Noah Rogers";
+            this.Load += new System.EventHandler(this.btnReset_Click);
+            this.grpBook.ResumeLayout(false);
+            this.grpBook.PerformLayout();
+            this.grpLocation.ResumeLayout(false);
+            this.grpLocation.PerformLayout();
+            this.grpTripInformation.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBook;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.TextBox txtPeople;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox grpTripInformation;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radCreditCard;
+        private System.Windows.Forms.RadioButton radCash;
+        private System.Windows.Forms.GroupBox grpLocation;
+        private System.Windows.Forms.CheckBox chkFlight;
+        private System.Windows.Forms.RadioButton radMexico;
+        private System.Windows.Forms.RadioButton radFlorida;
+        private System.Windows.Forms.RadioButton radCuba;
+    }
+}
+
---
+++ RogersLab4/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RogersLab4
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*By: Noah Rogers
+         *Due: Nov.17, 2022
+         *Description: This Program Calculates The total Cost for a trip comprising of 1 to 10 people for 3 possible locations.
+         *The program verifies that the user input is correct and displays appropriate error messages if the input is incorrect
+         *Once the user submits the booking, a display will show their selected options, the tallied price and an additional option
+         *that may or may not be applied based on the selections.
+        */
+
+        private void ResetTrip()
+        {
+            //hide groupbox
+            grpTripInformation.Hide();
+
+            //select cuba &  Credit Card radio buttons
+            radCuba.Checked = true;
+            radCreditCard.Checked = true;
+
+            //clear and foxus people textbox
+            txtPeople.Clear();
+            txtPeople.Focus();
+
+            //clear price label
+            lblPrice.Text = "";
+
+        }
+
+        private void SetFlight()
+        {
+            if (radFlorida.Checked)
+            {
+                chkFlight.Checked = false;
+            }
+            else
+            {
+                chkFlight.Checked = true;
+            }
+        }
+
+        private void DisplayMsg(string message, string title)
+        {
+            MessageBox.Show(message, title);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetTrip();
+        }
+
+        private void radFlorida_CheckedChanged(object sender, EventArgs e)
+        {
+            SetFlight();
+        }
+
+
+        private void radCuba_CheckedChanged(object sender, EventArgs e)
+        {
+            SetFlight();
+        }
+
+        private void radMexico_CheckedChanged(object sender, EventArgs e)
+        {
+            ResetTrip();
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            //constants declared
+            const double OtherPrice = 2150.50;
+            const double MexicoPrice = 2300.79;
+            const double Discount = 0.90;
+
+            //varibles
+            int People;
+            bool validPeople;
+            double Price;
+            string Applied;
+
+            //Location
+            string Location = "";
+            if(radMexico.Checked)
+                {
+                Location = "MEXICO";
+                    }
+            else if(radFlorida.Checked)
+                {
+                Location = "FLORIDA";
+                    }
+            else if(radCuba.Checked)
+                {
+                Location = "CUBA";
+                    }
+            else {
+                Location = "";
+                    }
+
+            if (chkFlight.Checked)
+                Applied = "Flight Included";
+            if (radCash.Checked)
+                Applied = "Cash Discount: Applied";
+
+            //vaildate that an integer is entered
+            validPeople = int.TryParse(txtPeople.Text, out People);
+
+            //if an integer is not entered...
+            if (validPeople != true)
+            {
+                //display this message
+                DisplayMsg("People must be a whole number", "Input Error");
+            }
+
+            //if an integer is outside the required range
+            else if (People < 1 || People > 10)
+            {
+                //display this message
+                DisplayMsg("People must be between 1-10", "input error");
+            }
+
+            else{
+
+                //if cash is checked
+                if (radCash.Checked)
+                {
+                    //use discount for both cases
+                    if (radMexico.Checked)
+                    {
+                        Price = People * MexicoPrice * Discount;
+                    }
+
+                    else
+                    {
+                        Price = People * OtherPrice * Discount;
+                    }
+
+                }
+
+                //if cash is unchecked
+                else
+                {
+                    //calculate both cases without discount
+                    if (radMexico.Checked)
+                    {
+                        Price = People * MexicoPrice;
+                    }
+
+                    else
+                    {
+                        Price = People * OtherPrice;
+                    }
+                }
+
+                grpTripInformation.Show();
+                lblDisplay.Text = "Booked by Noah Rogers\n\n" +
+                                  "People: " + People.ToString("D2") +
+                                  "\nLocation: " + Location + "\n"
+                                  + Applied + "\n"
+                                  + "Price: " + Price.ToString("C");
+                lblPrice.Text = Price.ToString("C");
+                grpBook.Enabled = false;
+
+
+                switch (People)
+                {
+                    case 1: case 3:
+                        DisplayMessage("Special when booking single or triple.\nBOGO Special - Call 555-1212 to receive another person free!", "Limited Time Offer");
+                        break;
+
+                }
+
+            }
+                
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            //Declare variable Price from label in previous Event
+            string Price = lblPrice.Text;
+
+            //Variable Used calling the DisplayMsg Function, reenable booking group and reset the form
+            DisplayMsg("Trip booked and paid\nPrice: " + Price, "Confirmation Email Sent");
+            grpBook.Enabled = true;
+            ResetTrip();
+
+        }
+    }
+}
